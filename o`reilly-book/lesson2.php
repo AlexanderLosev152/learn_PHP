ffccf9e2ff54a4dfa2a4f648fdaf2cec79086dad
Alexander L. <losev.pavlovo152@gmail.com>
2025-01-30 12:29:11 +0300

    commit4

--- o`reilly-book/lesson2.php
+++ o`reilly-book/lesson2.php
@@ -30,3 +30,58 @@ $month = 'Март';
 if ($month == 'Март') {
 	echo "Это месяц март <br>";
 }
+
+
+$page = "About";
+
+if ($page == "Home") {
+	echo "Вы выбрали домашнюю страницу <br>";
+} elseif ($page == "About") {
+	echo "Вы выбрали страницу о нас <br>";
+} elseif ($page == "News") {
+	echo "Вы выбрали страницу новостей <br>";
+} elseif ($page == "Login") {
+	echo "Вы выбрали страницу входа <br>";
+} else {
+	echo "Неизвестная страница <br>";
+}
+
+switch ($page) {
+	case "Home":
+		echo "Вы выбрали домашнюю страницу <br>";
+		break;
+	case "About":
+		echo "Вы выбрали страницу о нас <br>";
+		break;
+	case "News":
+		echo "Вы выбрали страницу новостей <br>";
+		break;
+	case "Login":
+		echo "Вы выбрали страницу входа <br>";
+		break;
+	case "default":
+		echo "Неизвестная страница <br>";
+		break;
+}
+
+$fuel = 10;
+echo $fuel <= 1 ? "Требуется дозаправка <br>" : "Топлива еше домтаточно <br>";
+
+$fuel = 10;
+while ($fuel > 1) {
+	echo "#";
+	$fuel++;
+}
+
+echo "<br>";
+for ($i = 1; $i <= 10; $i++) {
+	echo "# ";
+}
+
+$fp = fopen("text.txt", "wb");
+for ($j = 0; $j < 100; ++$j) {
+	$writen = fwrite($fp, "data");
+	if ($writen === false)
+		break;
+}
+fclose($fp);
