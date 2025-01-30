792a9051c367762c4355ec5d6f31387f4e89f438
Alexander L. <losev.pavlovo152@gmail.com>
2025-01-30 16:53:27 +0300

    str

---
+++ o`reilly-book/lesson3.php
@@ -0,0 +1,11 @@
+<?php
+
+echo date("1");
+
+// phpinfo();
+echo strrev(' .dlrow olleH'); // переворот текста
+echo str_repeat('Hip', 2); // повторение строки
+echo strtoupper('hoorey!'); // верхний регистр
+
+$lowered = strtolower(string: 'приВет кАк дЕлА!!!');
+echo $lowered;
